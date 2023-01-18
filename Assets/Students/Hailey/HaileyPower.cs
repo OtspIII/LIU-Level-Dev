79951b8ae476e3808694d000c6976053cfc8214b
Misha Favorov <otspiii@gmail.com>
2023-01-17 23:44:07 -0500

    Folders for each student

---
+++ Assets/Students/Hailey/HaileyPower.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HaileyPower : GenericPower
+{
+    public override void Activate()
+    {
+        //Insert code that runs when you hit 'z' here
+    }
+
+    void Update()
+    {
+
+    }
+}
