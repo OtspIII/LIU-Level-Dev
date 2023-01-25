d3642e8d8e8ae5acdb1a5ac9922d9921c709ac3a
fmorgan1413 <91088434+fmorgan1413@users.noreply.github.com>
2023-01-25 16:02:21 -0500

    initial power up

--- Assets/Students/Faith/FaithPower.cs
+++ Assets/Students/Faith/FaithPower.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public class FaithPower : GenericPower
 {
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private bool turnOff = true;
     public override void Activate()
     {
         //Insert code that runs when you hit 'z' here
+        rb = this.GetComponent<Rigidbody2D>();
+        //sr = 
+
+        if (turnOff) 
+        {
+            rb.gravityScale = 1;
+            //sr.flipX = false;
+
+            turnOff = false;
+        }
+        else 
+        { 
+            rb.gravityScale = 0;
+            //sr.flipX = true;
+
+            turnOff = true;
+        }
     }
 
     void Update()
