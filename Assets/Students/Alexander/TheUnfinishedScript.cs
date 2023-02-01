62a91194dbb9089f517a00d6e52bd355c346c0a1
Alexander Protopsaltis <Specterpants@gmail.com>
2023-02-01 14:13:37 -0500

    Whoops! Forgor to undo something.

--- Assets/Students/Alexander/TheUnfinishedScript.cs
+++ Assets/Students/Alexander/TheUnfinishedScript.cs
@@ -7,7 +7,7 @@ public class TheUnfinishedScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Hola
     }
 
     // Update is called once per frame
