44325283c3a059f3f7befb836b631a5a42292dc9
Aidan Byrne <ajbyrne99@gmail.com>
2023-02-01 14:13:00 -0500

    no message

--- Assets/Students/Aidan/PlayerScript.cs
+++ Assets/Students/Aidan/PlayerScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RedBrickRotate : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class RedBrickRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,0,5f);
+        
     }
 }
