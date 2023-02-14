b157a14ea943c7597b7703bf78adf7daeb6f74e2
snappylolz <113632664+snappylolz@users.noreply.github.com>
2023-02-14 14:35:16 -0500

    no message

--- Assets/Students/Isaias/IsaiasPower.cs
+++ Assets/Students/Isaias/IsaiasPower.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class IsaiasPower : GenericPower
 {
+    public float Timer = 0;
+    public float DashTime = 0.5f;
+    public float DashXSpeed = 30;
+    public float DashYSpeed = 0;
+
     public override void Activate()
     {
-        //Insert code that runs when you hit 'z' here
+        Timer = 1;
+        Player.SetInControl(false);
+        Player.SetGravity(0);
+        float dir = Player.FaceLeft ? -1 : 1;
+        Player.RB.velocity = new Vector2(DashXSpeed * dir, DashYSpeed);
     }
 
     void Update()
     {
-
+        if (Timer > 0)  
+        {
+            Timer -= Time.deltaTime / DashTime;
+            Player.Body.transform.rotation = Quaternion.Euler(0, 0, Timer * 360);
+            if (Timer <= 0)
+            {
+                Player.SetGravity(1);
+                Player.Body.transform.rotation = Quaternion.Euler(0,0,0);
+                Player.SetInControl(true);
+            }
+        }
     }
 }
