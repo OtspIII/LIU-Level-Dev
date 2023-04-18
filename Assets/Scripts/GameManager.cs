68cb4975268bcbd01cbd45e699122c68dcf8b38c
Misha Favorov <otspiii@gmail.com>
2023-04-17 20:55:09 -0400

    NPCs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,30 +6,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public NetworkManager NM;
-    public TextMeshProUGUI StatusText;
-    public NetStatus NStatus;
-    public string PlayerDebug;
+    public string Author;
 
     void Awake()
     {
         God.GM = this;
-        if (NM != null) God.NM = NM;
     }
     
     void Update()
     { 
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            NM.StartClient();
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            NM.StartHost();
-        }
-
-        NStatus = God.GetStatus();
-        StatusText.text = NStatus.ToString() + " / " + PlayerDebug;
+       
     }
     
     
